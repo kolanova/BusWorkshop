dc57e6a61432b8196960e3439fb7973c73836656
Tomasabernardo <ab.tomas@hotmail.com>
2021-10-19 17:41:25 +0100

    GET endpoints commit

--- src/main/java/com/BusWorkshop/controller/BusController.java
+++ src/main/java/com/BusWorkshop/controller/BusController.java
@@ -1,13 +1,43 @@
 package com.BusWorkshop.controller;
+import com.BusWorkshop.model.Bus;
+import com.BusWorkshop.model.MaintenanceTeam;
 import com.BusWorkshop.services.BusService;
+import com.BusWorkshop.services.MaintenanceTeamService;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class BusController {
 
     BusService busService;
+    MaintenanceTeamService maintenanceTeamService;
+
+
 
     public BusController(BusService busService) {
         this.busService = busService;
     }
+    public BusController(MaintenanceTeamService maintenanceTeamService) {this.maintenanceTeamService = maintenanceTeamService;}
+
+    // Get all buses on database
+    @GetMapping("/buses")
+    public List<Bus> getBuses() {
+        return busService.findAll();}
+    // Get all maintenance teams on database
+    @GetMapping("/maintenanceteams")
+    public List<MaintenanceTeam> getMaintenanceTeam() {
+        return maintenanceTeamService.findAll();}
+    // Get all buses on database by ID
+    @GetMapping("/buses/{id}")
+    public Optional<Bus> getBusesId(String id) {
+        return busService.findById(id);}
+    // Get all maintenance teams on database by ID
+    @GetMapping("/maintenanceteams/{id}")
+    public Optional<MaintenanceTeam> getMaintenanceTeamId(String id) {
+        return maintenanceTeamService.findById(id);
+    }
+
 }
--- src/main/java/com/BusWorkshop/services/MaintenanceTeamService.java
+++ src/main/java/com/BusWorkshop/services/MaintenanceTeamService.java
@@ -1,6 +1,10 @@
 package com.BusWorkshop.services;
+import com.BusWorkshop.model.MaintenanceTeam;
 import com.BusWorkshop.repository.MaintenanceTeamRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class MaintenanceTeamService {
@@ -10,4 +14,11 @@ public class MaintenanceTeamService {
     public MaintenanceTeamService(MaintenanceTeamRepository maintenanceTeamRepository) {
         this.maintenanceTeamRepository = maintenanceTeamRepository;
     }
+    public List<MaintenanceTeam> findAll() {
+        return maintenanceTeamRepository.findAll();
+    }
+    public Optional<MaintenanceTeam>  findById(String id) {
+        return maintenanceTeamRepository.findById(id);
+    }
+
 }
